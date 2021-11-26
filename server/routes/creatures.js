0ecb75341170be1481d3cf52ae040d849fa7d370
Hannah Park <hannahp056@gmail.com>
2021-11-26 13:49:06 -0500

    create post endpoint to create a new creature

--- server/routes/creatures.js
+++ server/routes/creatures.js
@@ -21,12 +21,32 @@ const getCreatures = () => {
 
 getCreatures();
 
-// const writeCreatures = (data) => {
-//   fs.writeFileSync(path.resolve(__dirname, "../data/"), JSON.stringify(data));
-// };
+const writeCreatures = (data) => {
+  fs.writeFileSync(
+    path.resolve(__dirname, "../data/creatures.json"),
+    JSON.stringify(data)
+  );
+};
 
 router.get("/", (req, res) => {
   res.json(creatures);
+});
+
+router.post("/", (req, res) => {
+  const { name, head, body, legs } = req.body;
+
+  const newCreature = {
+    id: uuid.v4(),
+    name: name,
+    head: head,
+    body: body,
+    legs: legs,
+  };
+
+  creatures.push(newCreature);
+  writeCreatures(creatures);
+
+  res.json(newCreature);
 });
 
 module.exports = router;
