334e02d21ae0a83e6ee9e0b9bbda2051a1023840
Hannah Park <hannahp056@gmail.com>
2021-11-27 10:45:08 -0500

    make basic create page structure

--- client/src/pages/Create/Create.js
+++ client/src/pages/Create/Create.js
@@ -1,5 +1,37 @@
 import "./Create.scss";
 
 export default function Create() {
-  return <h2>This is the create page</h2>;
+  return (
+    <section className="create">
+      <h2 className="create__title">Create your creature!</h2>
+      <label htmlFor="name" className="create__label">
+        Name:
+      </label>
+      <input
+        id="name"
+        type="text"
+        placeholder="give your creature a name"
+        className="create__input"
+      ></input>
+      <section className="create__machine">
+        <div className="create__machine-sections">
+          <div className="create__machine-section">
+            <svg width="200" height="100"></svg>
+          </div>
+          <div className="create__machine-section">
+            <svg width="200" height="150"></svg>
+          </div>
+          <div className="create__machine-section">
+            <svg width="200" height="150"></svg>
+          </div>
+        </div>
+        <div className="create__machine-buttons">
+          <button className="create__machine-button">Change head</button>
+          <button className="create__machine-button">Change body</button>
+          <button className="create__machine-button">Change legs</button>
+        </div>
+      </section>
+      <button clasName="create__create-button">Create!</button>
+    </section>
+  );
 }
