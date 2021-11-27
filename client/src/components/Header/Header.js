64aef3a6470679158d9badfb81bd1ca77a58c001
Hannah Park <hannahp056@gmail.com>
2021-11-27 10:15:55 -0500

    styled header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,27 @@
-import "./Header.scss"
-import { Link } from "react-router-dom"
+import "./Header.scss";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-return (
-  <header>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/create">Create</Link>
-      <Link to="/gallery">Gallery</Link>
-    </nav>
-  </header>
-);
+  return (
+    <header className="header">
+      <Link to="/" className="header__logo-link">
+        <div className="header__logo-outline">
+          <div className="header__logo-container">
+            {" "}
+            <h3 className="header__logo">
+              Creature <br /> Factory
+            </h3>
+          </div>
+        </div>
+      </Link>
+      <nav className="header__nav">
+        <Link to="/create" className="header__nav-link">
+          Create
+        </Link>
+        <Link to="/gallery" className="header__nav-link">
+          Gallery
+        </Link>
+      </nav>
+    </header>
+  );
 }
