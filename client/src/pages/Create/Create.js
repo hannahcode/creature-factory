111d5180c48f6f592335a8989e7481c6459cddd7
Hannah Park <hannahp056@gmail.com>
2021-11-27 11:28:19 -0500

    create test body part components

--- client/src/pages/Create/Create.js
+++ client/src/pages/Create/Create.js
@@ -1,4 +1,10 @@
 import "./Create.scss";
+import TestHead from "../../components/TestHead/TestHead";
+import TestBody from "../../components/TestBody/TestBody";
+import TestLegs from "../../components/TestLegs/TestLegs";
+import TestHead2 from "../../components/TestHead2/TestHead2";
+import TestBody2 from "../../components/TestBody2/TestBody2";
+import TestLegs2 from "../../components/TestLegs2/TestLegs2";
 
 export default function Create() {
   return (
@@ -14,16 +20,15 @@ export default function Create() {
         className="create__input"
       ></input>
       <section className="create__machine">
-        <div className="create__machine-sections">
-          <div className="create__machine-section">
-            <svg width="200" height="100"></svg>
-          </div>
-          <div className="create__machine-section">
-            <svg width="200" height="150"></svg>
-          </div>
-          <div className="create__machine-section">
-            <svg width="200" height="150"></svg>
-          </div>
+        <div className="create__machine-parts-section">
+          <svg width="200" height="500">
+            <TestLegs />
+            <TestLegs2 />
+            <TestBody />
+            <TestBody2 />
+            <TestHead />
+            <TestHead2 />
+          </svg>
         </div>
         <div className="create__machine-buttons">
           <button className="create__machine-button">Change head</button>
