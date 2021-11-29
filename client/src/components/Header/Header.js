2ec47f3df8a0fca4dc0183360c172021b9ced283
Hannah Park <hannahp056@gmail.com>
2021-11-28 20:22:54 -0500

    add responsive styling to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,24 +4,26 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="header">
-      <Link to="/" className="header__logo-link">
-        <div className="header__logo-outline">
-          <div className="header__logo-container">
-            {" "}
-            <h3 className="header__logo">
-              Creature <br /> Factory
-            </h3>
+      <div className="header__content-container">
+        <Link to="/" className="header__logo-link">
+          <div className="header__logo-outline">
+            <div className="header__logo-container">
+              {" "}
+              <h3 className="header__logo">
+                Creature <br /> Factory
+              </h3>
+            </div>
           </div>
-        </div>
-      </Link>
-      <nav className="header__nav">
-        <Link to="/create" className="header__nav-link">
-          Create
         </Link>
-        <Link to="/gallery" className="header__nav-link">
-          Gallery
-        </Link>
-      </nav>
+        <nav className="header__nav">
+          <Link to="/create" className="header__nav-link">
+            Create
+          </Link>
+          <Link to="/gallery" className="header__nav-link">
+            Gallery
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
