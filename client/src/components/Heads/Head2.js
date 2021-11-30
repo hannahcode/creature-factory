0b99560512e7cb615b92fc219ff8f6405bb914ed
Hannah Park <hannahp056@gmail.com>
2021-11-30 18:38:18 -0500

    create new head part component

---
+++ client/src/components/Heads/Head2.js
@@ -0,0 +1,234 @@
+export default function Head2() {
+  return (
+    <g transform="translate(50, 5)">
+      <circle cx="50" cy="50" r="45" fill="black" />
+      <g>
+        <polygon points="80 30, 90 40, 105 20 " fill="black" />
+        <polygon
+          points="80 30, 90 40, 105 20 "
+          fill="black"
+          transform="translate(3, 6) rotate(5)"
+        />
+        <polygon
+          points="80 30, 90 40, 105 20 "
+          fill="black"
+          transform="translate(5, 10) rotate(10)"
+        />
+        <g transform="translate(14, -10) rotate(20)">
+          <polygon points="80 30, 90 40, 105 20 " fill="black" />
+          <polygon
+            points="80 30, 90 40, 105 20 "
+            fill="black"
+            transform="translate(3, 6) rotate(5)"
+          />
+          <polygon
+            points="80 30, 90 40, 105 20 "
+            fill="black"
+            transform="translate(5, 10) rotate(10)"
+          />
+        </g>
+      </g>
+      <g transform="translate(-10, -5)">
+        <polygon points="80 30, 90 40, 105 20 " fill="black" />
+        <polygon
+          points="80 30, 90 40, 105 20 "
+          fill="black"
+          transform="translate(3, 6) rotate(5)"
+        />
+        <polygon
+          points="80 30, 90 40, 105 20 "
+          fill="black"
+          transform="translate(5, 10) rotate(10)"
+        />
+        <g transform="translate(14, -10) rotate(20)">
+          <polygon points="80 30, 90 40, 105 20 " fill="black" />
+          <polygon
+            points="80 30, 90 40, 105 20 "
+            fill="black"
+            transform="translate(3, 6) rotate(5)"
+          />
+          <polygon
+            points="80 30, 90 40, 105 20 "
+            fill="black"
+            transform="translate(5, 10) rotate(10)"
+          />
+        </g>
+      </g>
+      <g transform="">
+        <polygon points="10 40, -5 20, 20 30" fill="black" />
+        <polygon
+          points="10 40, -5 20, 20 30"
+          fill="black"
+          transform="translate(-3, 13) rotate(-5)"
+        />
+        <polygon
+          points="10 40, -5 20, 20 30"
+          fill="black"
+          transform="translate(-5, 24) rotate(-10)"
+        />
+      </g>
+      <g transform="translate(-10,20) rotate(-20)">
+        <polygon points="10 40, -5 20, 20 30" fill="black" />
+        <polygon
+          points="10 40, -5 20, 20 30"
+          fill="black"
+          transform="translate(-3, 13) rotate(-5)"
+        />
+        <polygon
+          points="10 40, -5 20, 20 30"
+          fill="black"
+          transform="translate(-5, 24) rotate(-10)"
+        />
+      </g>
+      <g>
+        <g transform="translate(10, -10)">
+          <polygon points="10 40, -5 20, 20 30" fill="black" />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-3, 13) rotate(-5)"
+          />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-5, 24) rotate(-10)"
+          />
+        </g>
+        <g transform="translate(-10,20) rotate(-20)">
+          <polygon points="10 40, -5 20, 20 30" fill="black" />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-3, 13) rotate(-5)"
+          />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-5, 24) rotate(-10)"
+          />
+        </g>
+      </g>
+      <g transform="rotate(72) translate(17, -88)">
+        <g transform="translate(10, -10)">
+          <polygon points="10 40, -5 20, 20 30" fill="black" />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-3, 13) rotate(-5)"
+          />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-5, 24) rotate(-10)"
+          />
+        </g>
+        <g transform="translate(-10,20) rotate(-20)">
+          <polygon points="10 40, -5 20, 20 30" fill="black" />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-3, 13) rotate(-5)"
+          />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-5, 24) rotate(-10)"
+          />
+        </g>
+      </g>
+      <g transform="rotate(85) translate(10, -100)">
+        <g transform="translate(10, -10)">
+          <polygon points="10 40, -5 20, 20 30" fill="black" />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-3, 13) rotate(-5)"
+          />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-5, 24) rotate(-10)"
+          />
+        </g>
+        <g transform="translate(-10,20) rotate(-20)">
+          <polygon points="10 40, -5 20, 20 30" fill="black" />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-3, 13) rotate(-5)"
+          />
+          <polygon
+            points="10 40, -5 20, 20 30"
+            fill="black"
+            transform="translate(-5, 24) rotate(-10)"
+          />
+        </g>
+      </g>
+      <polygon points="1 -3, 35 13, 5 35" fill="#495057" />
+      <polygon points="3 0, 37 15, 6 35" fill="black" />
+      <polygon points="7 6, 28 15, 10 30" fill="#ff4d6d" />
+      <polygon points="99 -3, 65 13, 95 35" fill="#495057" />
+      <polygon points="97 0, 63 15, 94 35" fill="black" />
+      <polygon points="93 6, 72 15, 90 30" fill="#ff4d6d" />
+      <g transform="translate(-10, -5)">
+        <mask id="eyeballMask">
+          <ellipse
+            cx="50"
+            cy="50"
+            rx="10"
+            ry="5"
+            fill="white"
+            transform="rotate(15)"
+          />
+        </mask>
+        <ellipse
+          cx="50"
+          cy="50"
+          rx="10"
+          ry="5"
+          fill="white"
+          transform="rotate(15)"
+        />
+        <circle
+          cx="40"
+          cy="63"
+          r="6"
+          fill="#a8dadc"
+          mask="url(#eyeballMask)"
+          strokeWidth="1px"
+          stroke="black"
+        />
+        <circle cx="43" cy="63" r="4" fill="black" mask="url(#eyeballMask)" />
+        <mask id="eyeballMask2">
+          <ellipse
+            cx="65"
+            cy="80"
+            rx="10"
+            ry="5"
+            fill="white"
+            transform="rotate(-15)"
+          />
+        </mask>
+        <ellipse
+          cx="65"
+          cy="80"
+          rx="10"
+          ry="5"
+          fill="white"
+          transform="rotate(-15)"
+        />
+        <circle
+          cx="80"
+          cy="63"
+          r="6"
+          fill="#a8dadc"
+          mask="url(#eyeballMask2)"
+          strokeWidth="1px"
+          stroke="black"
+        />
+        <circle cx="78" cy="63" r="4" fill="black" mask="url(#eyeballMask2)" />
+        <circle cx="60" cy="70" r="3" fill="#495057" />
+      </g>
+    </g>
+  );
+}
--- client/src/components/Heads/index.js
+++ client/src/components/Heads/index.js
@@ -1,9 +1,11 @@
 import Head1 from "./Head1";
+import Head2 from "./Head2";
 import TestHead from "./TestHead";
 import TestHead2 from "./TestHead2";
 
 export const Heads = {
   head1: <Head1 />,
-  head2: <TestHead />,
-  head3: <TestHead2 />
+  head2: <Head2 />,
+  head3: <TestHead />,
+  head4: <TestHead2 />,
 };
