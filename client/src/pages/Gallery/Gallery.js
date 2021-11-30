5bc4df4ddb20de38fe3d1ca9a0fa1c86fd6fb4d5
Hannah Park <hannahp056@gmail.com>
2021-11-30 15:36:59 -0500

    refactor gallery page to render creatures from parts objects

--- client/src/pages/Gallery/Gallery.js
+++ client/src/pages/Gallery/Gallery.js
@@ -1,18 +1,12 @@
 import "./Gallery.scss";
-import TestHead from "../../components/Heads/TestHead";
-import TestBody from "../../components/Bodies/TestBody";
-import TestLegs from "../../components/Legs/TestLegs";
-import TestHead2 from "../../components/Heads/TestHead2";
-import TestBody2 from "../../components/Bodies/TestBody2";
-import TestLegs2 from "../../components/Legs/TestLegs2";
+import { Heads } from "../../components/Heads";
+import { Bodies } from "../../components/Bodies";
+import { Legs } from "../../components/Legs";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const apiUrl = "http://localhost:8081/creatures/";
 
-const headArray = [<TestHead />, <TestHead2 />];
-const bodyArray = [<TestBody />, <TestBody2 />];
-const legsArray = [<TestLegs />, <TestLegs2 />];
 
 export default function Gallery() {
   const [creatures, setCreatures] = useState([]);
@@ -34,9 +28,9 @@ export default function Gallery() {
               <h2 className="gallery__creature-title">{creature.name}</h2>
               <svg width="133.33" height="233.33">
                 <g transform="scale(.66)">
-                  {legsArray[creature.legs]}
-                  {bodyArray[creature.body]}
-                  {headArray[creature.head]}
+                  {Legs[creature.legs]}
+                  {Bodies[creature.body]}
+                  {Heads[creature.head]}
                 </g>
               </svg>
             </div>
