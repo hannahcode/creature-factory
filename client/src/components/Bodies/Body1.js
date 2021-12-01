541ca1a6712ec55f0002a4f862fcfe783477225d
Hannah Park <hannahp056@gmail.com>
2021-11-30 22:41:15 -0500

    add new body part component

---
+++ client/src/components/Bodies/Body1.js
@@ -0,0 +1,133 @@
+export default function Body1() {
+  return (
+    <g transform="translate(50, 85)">
+      <rect
+        x="2.5"
+        y="3"
+        rx="10"
+        ry="10"
+        width="25"
+        height="100"
+        fill="#caffbf"
+        strokeWidth="1.5"
+        transform="rotate(50) translate(20,-30)"
+      />
+      <rect
+        x="74.5"
+        y="3"
+        rx="10"
+        ry="10"
+        width="25"
+        height="100"
+        fill="#caffbf"
+        strokeWidth="1.5"
+        transform="rotate(-50) translate(-58,45)"
+      />
+      <rect
+        x="12.5"
+        y="1"
+        rx="10"
+        ry="10"
+        width="75"
+        height="120"
+        fill="#caffbf"
+        strokeWidth="1.5"
+      />
+      <circle cx="85" cy="23" r="10" fill="#caffbf" />
+      <circle cx="15" cy="23" r="10" fill="#caffbf" />
+      <circle cx="95" cy="34" r="10" fill="#caffbf" />
+      <circle cx="5" cy="34" r="10" fill="#caffbf" />
+      <circle cx="82" cy="80" r="10" fill="#caffbf" />
+      <circle cx="18" cy="80" r="10" fill="#caffbf" />
+      <circle cx="82" cy="95" r="10" fill="#caffbf" />
+      <circle cx="18" cy="95" r="10" fill="#caffbf" />
+      <polygon points="-35 70, -28 76, -40 90 " />
+      <polygon points="128 75, 135 69, 140 90" />
+      <circle
+        cx="-25"
+        cy="76"
+        r="3.5"
+        fill="#bdb2ff"
+        strokeWidth="2px"
+        stroke="#9bf6ff"
+      />
+      <circle
+        cx="-30"
+        cy="71"
+        r="3.5"
+        fill="#bdb2ff"
+        strokeWidth="2px"
+        stroke="#9bf6ff"
+      />
+      <circle
+        cx="-35"
+        cy="66.5"
+        r="3.5"
+        fill="#bdb2ff"
+        strokeWidth="2px"
+        stroke="#9bf6ff"
+      />
+      <circle
+        cx="125"
+        cy="76"
+        r="3.5"
+        fill="#bdb2ff"
+        strokeWidth="2px"
+        stroke="#9bf6ff"
+      />
+      <circle
+        cx="130"
+        cy="71"
+        r="3.5"
+        fill="#bdb2ff"
+        strokeWidth="2px"
+        stroke="#9bf6ff"
+      />
+      <circle
+        cx="135"
+        cy="66.5"
+        r="3.5"
+        fill="#bdb2ff"
+        strokeWidth="2px"
+        stroke="#9bf6ff"
+      />
+      <rect
+        x="25"
+        y="15"
+        rx="50"
+        ry="10"
+        width="50"
+        height="100"
+        fill="#bdb2ff"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M 25 50 C 40 60, 60 60, 75 50"
+        stroke="black"
+        strokeWidth="1.5px"
+        fill="transparent"
+      />
+      <path
+        d="M 25 50 C 40 60, 60 60, 75 50"
+        stroke="black"
+        strokeWidth="1.5px"
+        fill="transparent"
+        transform="translate(0, -20)"
+      />
+      <path
+        d="M 25 50 C 40 60, 60 60, 75 50"
+        stroke="black"
+        strokeWidth="1.5px"
+        fill="transparent"
+        transform="translate(0, 20)"
+      />
+      <path
+        d="M 25 50 C 40 60, 60 60, 75 50"
+        stroke="black"
+        strokeWidth="1.5px"
+        fill="transparent"
+        transform="translate(0, 40)"
+      />
+    </g>
+  );
+}
--- client/src/components/Bodies/index.js
+++ client/src/components/Bodies/index.js
@@ -1,7 +1,9 @@
 import TestBody from "./TestBody";
 import TestBody2 from "./TestBody2";
+import Body1 from "./Body1";
 
 export const Bodies = {
-  body1: <TestBody />,
-  body2: <TestBody2 />,
+  body1: <Body1 />,
+  body2: <TestBody />,
+  body3: <TestBody2 />,
 };
