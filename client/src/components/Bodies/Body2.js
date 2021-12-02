ad474d4401ae1a6e0844632c9fe5124bbc265dbd
Hannah Park <hannahp056@gmail.com>
2021-12-01 21:40:35 -0500

    added new body part component

---
+++ client/src/components/Bodies/Body2.js
@@ -0,0 +1,172 @@
+export default function Body2() {
+  return (
+    <g transform="translate(50, 85)">
+      <rect
+        x="2.5"
+        y="3"
+        rx="10"
+        ry="10"
+        width="15"
+        height="100"
+        fill="#8eecf5"
+        transform="rotate(50) translate(22,-30)"
+      />
+      <rect
+        x="74.5"
+        y="3"
+        rx="10"
+        ry="10"
+        width="15"
+        height="100"
+        fill="#8eecf5"
+        transform="rotate(-50) translate(-50,45)"
+      />
+      <g transform="translate(0, 30)">
+        <rect
+          x="2.5"
+          y="3"
+          rx="10"
+          ry="10"
+          width="15"
+          height="100"
+          fill="#8eecf5"
+          transform="rotate(50) translate(22,-30)"
+        />
+        <rect
+          x="74.5"
+          y="3"
+          rx="10"
+          ry="10"
+          width="15"
+          height="100"
+          fill="#8eecf5"
+          transform="rotate(-50) translate(-50,45)"
+        />
+      </g>
+      <rect
+        x="12.5"
+        y="1"
+        rx="10"
+        ry="10"
+        width="75"
+        height="120"
+        fill="#8eecf5"
+      />
+      <rect
+        x="21"
+        y="56"
+        rx="10"
+        ry="10"
+        width="21"
+        height="30"
+        fill="#ffcfd2"
+        transform="rotate(50)"
+      />
+      <rect
+        x="45"
+        y="70"
+        rx="10"
+        ry="10"
+        width="21"
+        height="30"
+        fill="#ffcfd2"
+        transform="rotate(50)"
+      />
+      <rect
+        x="-1"
+        y="145"
+        rx="10"
+        ry="10"
+        width="21"
+        height="30"
+        fill="#ffcfd2"
+        transform="rotate(-50)"
+      />
+      <rect
+        x="23"
+        y="133"
+        rx="10"
+        ry="10"
+        width="21"
+        height="30"
+        fill="#ffcfd2"
+        transform="rotate(-50)"
+      />
+      <circle cx="-31" cy="66" r="6" fill="yellow" />
+      <circle cx="-35" cy="78" r="2" fill="yellow" />
+      <circle cx="-40" cy="75.5" r="2" fill="yellow" />
+      <circle cx="-43" cy="71" r="2" fill="yellow" />
+      <g transform="translate(5,28)">
+        <circle cx="-31" cy="66" r="6" fill="yellow" />
+        <circle cx="-35" cy="78" r="2" fill="yellow" />
+        <circle cx="-40" cy="75.5" r="2" fill="yellow" />
+        <circle cx="-43" cy="71" r="2" fill="yellow" />
+      </g>
+      <g transform="translate(65,35) rotate(-90)">
+        <circle cx="-31" cy="66" r="6" fill="yellow" />
+        <circle cx="-35" cy="78" r="2" fill="yellow" />
+        <circle cx="-40" cy="75.5" r="2" fill="yellow" />
+        <circle cx="-43" cy="71" r="2" fill="yellow" />
+      </g>
+      <g transform="translate(59,61) rotate(-90)">
+        <circle cx="-31" cy="66" r="6" fill="yellow" />
+        <circle cx="-35" cy="78" r="2" fill="yellow" />
+        <circle cx="-40" cy="75.5" r="2" fill="yellow" />
+        <circle cx="-43" cy="71" r="2" fill="yellow" />
+      </g>
+      <g transform="translate(92,67) scale(.5)">
+        <polyline
+          points="0,20 20,20 20,40 40,40 40,60 60,60 60,80"
+          fill="#8eecf5"
+          transform="rotate(45)"
+        />
+      </g>
+      <g transform="translate(8,123) scale(.5)">
+        <polyline
+          points="0,20 20,20 20,40 40,40 40,60 60,60 60,80"
+          fill="#8eecf5"
+          transform="rotate(-135)"
+        />
+      </g>
+      <g transform="translate(0,-46)">
+        <g transform="translate(92,67) scale(.5)">
+          <polyline
+            points="0,20 20,20 20,40 40,40 40,60 60,60 60,80"
+            fill="#8eecf5"
+            transform="rotate(45)"
+          />
+        </g>
+        <g transform="translate(8,123) scale(.5)">
+          <polyline
+            points="0,20 20,20 20,40 40,40 40,60 60,60 60,80"
+            fill="#8eecf5"
+            transform="rotate(-135)"
+          />
+        </g>
+      </g>
+      <rect
+        x="20"
+        y="20"
+        rx="30"
+        ry="50"
+        width="60"
+        height="90"
+        fill="yellow"
+      />
+      <g transform="translate(17,95)">
+        <polyline
+          points="0,20 20,20 20,40 40,40 40,60 60,60 60,80"
+          fill="yellow"
+          transform="rotate(-135) scale(.5)"
+        />
+      </g>
+      <g transform="translate(82.5, 38.5)">
+        <polyline
+          points="0,20 20,20 20,40 40,40 40,60 60,60 60,80"
+          fill="yellow"
+          transform="rotate(45) scale(.5)"
+        />
+      </g>
+    </g>
+  );
+}
--- client/src/components/Bodies/index.js
+++ client/src/components/Bodies/index.js
@@ -1,9 +1,9 @@
-import TestBody from "./TestBody";
 import TestBody2 from "./TestBody2";
 import Body1 from "./Body1";
+import Body2 from "./Body2";
 
 export const Bodies = {
   body1: <Body1 />,
-  body2: <TestBody />,
+  body2: <Body2 />,
   body3: <TestBody2 />,
 };
