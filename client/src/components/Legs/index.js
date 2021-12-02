8f770b67d5893cdc2836b581963a961758e07b5f
Hannah Park <hannahp056@gmail.com>
2021-12-02 10:29:57 -0500

    add new legs component

--- client/src/components/Legs/index.js
+++ client/src/components/Legs/index.js
@@ -1,8 +1,9 @@
-
 import TestLegs from "./TestLegs";
 import TestLegs2 from "./TestLegs2";
+import Legs3 from "./Legs3";
 
 export const Legs = {
   legs1: <TestLegs />,
   legs2: <TestLegs2 />,
+  legs3: <Legs3 />,
 };
