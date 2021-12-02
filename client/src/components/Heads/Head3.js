37f58fccbc631f97419607676a55137e677652ac
Hannah Park <hannahp056@gmail.com>
2021-12-02 15:05:11 -0500

    add new head component

---
+++ client/src/components/Heads/Head3.js
@@ -0,0 +1,73 @@
+export default function Head3() {
+  return (
+    <g transform="translate(50, 0)">
+      <circle cx="95" cy="54" r="10" fill="#ffb703" stroke="#4cc9f0" />
+      <circle cx="5" cy="54" r="10" fill="#ffb703" stroke="#4cc9f0" />
+      <circle cx="50" cy="12" r="10" fill="#ffb703" stroke="#4cc9f0" />
+      <circle cx="50" cy="54" r="45" fill="#ffb703" />
+      <rect x="90" y="50" rx="2" ry="2" width="15" height="2" fill="#4cc9f0" />
+      <rect x="90" y="55" rx="2" ry="2" width="15" height="2" fill="#4cc9f0" />
+      <rect x="-5" y="50" rx="2" ry="2" width="15" height="2" fill="#4cc9f0" />
+      <rect x="-5" y="55" rx="2" ry="2" width="15" height="2" fill="#4cc9f0" />
+      <rect x="52" y="2" rx="2" ry="2" width="2" height="7" fill="#4cc9f0" />
+      <rect x="48" y="2" rx="2" ry="2" width="2" height="7" fill="#4cc9f0" />
+      <circle
+        cx="33"
+        cy="50"
+        r="8"
+        stroke="#4cc9f0"
+        strokeWidth="1.5"
+        fill="white"
+      />
+      <circle
+        cx="67"
+        cy="50"
+        r="8"
+        stroke="#4cc9f0"
+        strokeWidth="1.5"
+        fill="white"
+      />
+      <circle
+        cx="33"
+        cy="50"
+        r="5"
+        stroke="#4cc9f0"
+        strokeWidth="1.5"
+        fill="black"
+      />
+      <circle
+        cx="67"
+        cy="50"
+        r="5"
+        stroke="#4cc9f0"
+        strokeWidth="1.5"
+        fill="black"
+      />
+      <circle
+        cx="34"
+        cy="49"
+        r="2"
+        stroke="black"
+        strokeWidth="1.5"
+        fill="white"
+      />
+      <circle
+        cx="68"
+        cy="49"
+        r="2"
+        stroke="black"
+        strokeWidth="1.5"
+        fill="white"
+      />
+      <path
+        d="M 35 65 C 60 70, 60 60, 70 65"
+        stroke="#4cc9f0"
+        strokeWidth="1.5"
+        fill="transparent"
+      />
+      <polygon points="60,65 65,64 60,75" fill="white" stroke="#4cc9f0" />
+      <circle cx="27" cy="64" r="4" fill="#f72585" />
+      <circle cx="76" cy="64" r="4" fill="#f72585" />
+    </g>
+  );
+}
--- client/src/components/Heads/index.js
+++ client/src/components/Heads/index.js
@@ -2,10 +2,11 @@ import Head1 from "./Head1";
 import Head2 from "./Head2";
 import TestHead from "./TestHead";
 import TestHead2 from "./TestHead2";
+import Head3 from "./Head3";
 
 export const Heads = {
   head1: <Head1 />,
   head2: <Head2 />,
-  head3: <TestHead />,
+  head3: <Head3 />,
   head4: <TestHead2 />,
 };
