910bcd5195eae025bed00567b341ad35d0fb9e69
Hannah Park <hannahp056@gmail.com>
2021-12-03 15:06:19 -0500

    edit index.js in server with conditional logic for production

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const creatureRoutes = require("./routes/creatures");
 const PORT = process.env.PORT || 8081;
 const cors = require("cors");
@@ -10,6 +10,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/creatures", creatureRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server up and running on Port ${PORT}`);
