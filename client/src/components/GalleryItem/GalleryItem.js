6d8c95df4329ad01fca5fd51992bd26c874394a0
Hannah Park <hannahp056@gmail.com>
2021-12-05 18:18:53 -0500

    start fixing download bug - make gallery item its own component

---
+++ client/src/components/GalleryItem/GalleryItem.js
@@ -0,0 +1,68 @@
+import "./GalleryItem.scss";
+import { Heads } from "../../components/Heads";
+import { Bodies } from "../../components/Bodies";
+import { Legs } from "../../components/Legs";
+import upvote from "../../assets/icons/expand_less_black_24dp.svg";
+import downvote from "../../assets/icons/expand_more_black_24dp.svg";
+// import html2canvas from "html2canvas";
+
+
+export default function GalleryItem({
+  creature,
+  handleDownvote,
+  handleUpvote,
+}) {
+  // const handleDownloadImage = async () => {
+  //   const element = printRef.current;
+  //   const canvas = await html2canvas(element);
+
+  //   const data = canvas.toDataURL("image/png");
+  //   const link = document.createElement("a");
+
+  //   if (typeof link.download === "string") {
+  //     link.href = data;
+  //     link.download = "image.png";
+
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     document.body.removeChild(link);
+  //   } else {
+  //     window.open(data);
+  //   }
+  // };
+
+  return (
+    <div key={creature.id} className="gallery__creature">
+      <div className="gallery__print-container">
+        <h2 className="gallery__creature-title">{creature.name}</h2>
+        <svg width="133.33" height="233.33">
+          <g transform="scale(.66)">
+            {Legs[creature.legs]}
+            {Bodies[creature.body]}
+            {Heads[creature.head]}
+          </g>
+        </svg>
+      </div>
+      <button type="button" className="gallery__download-button">
+        Download Creature
+      </button>
+      <div className="gallery__creature-likes-container">
+        <img
+          src={upvote}
+          alt="up arrow to increase likes"
+          className="gallery__creature-upvote"
+          onClick={() => handleDownvote(creature.id)}
+          id={creature.id}
+        />
+        <p className="gallery__creature-likes">{creature.likes}</p>
+        <img
+          src={downvote}
+          alt="down arrow to decrease likes"
+          className="gallery__creature-downvote"
+          onClick={() => handleUpvote(creature.id)}
+          id={creature.id}
+        />
+      </div>
+    </div>
+  );
+}
--- client/src/pages/Gallery/Gallery.js
+++ client/src/pages/Gallery/Gallery.js
@@ -1,17 +1,11 @@
 import "./Gallery.scss";
-import { Heads } from "../../components/Heads";
-import { Bodies } from "../../components/Bodies";
-import { Legs } from "../../components/Legs";
-import upvote from "../../assets/icons/expand_less_black_24dp.svg";
-import downvote from "../../assets/icons/expand_more_black_24dp.svg";
+import GalleryItem from "../../components/GalleryItem/GalleryItem";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import html2canvas from "html2canvas";
 import { API_URL } from "../../config";
 
 export default function Gallery() {
   const [creatures, setCreatures] = useState([]);
-  const printRef = React.useRef();
 
   const getCreatures = () => {
     axios
@@ -28,25 +22,6 @@ export default function Gallery() {
     getCreatures();
   }, []);
 
-  const handleDownloadImage = async () => {
-    const element = printRef.current;
-    const canvas = await html2canvas(element);
-
-    const data = canvas.toDataURL("image/png");
-    const link = document.createElement("a");
-
-    if (typeof link.download === "string") {
-      link.href = data;
-      link.download = "image.png";
-
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    } else {
-      window.open(data);
-    }
-  };
-
   const handleUpvote = (event) => {
     axios
       .put(`${API_URL}/${event.target.id}/upvote`)
@@ -58,9 +33,9 @@ export default function Gallery() {
       });
   };
 
-  const handleDownvote = (event) => {
+  const handleDownvote = (id) => {
     axios
-      .put(`${API_URL}/${event.target.id}/downvote`)
+      .put(`${API_URL}/${id}/downvote`)
       .then(() => {
         getCreatures();
       })
@@ -75,42 +50,11 @@ export default function Gallery() {
       <section className="gallery__creatures">
         {creatures.map((creature) => {
           return (
-            <div key={creature.id} className="gallery__creature">
-              <div className="gallery__print-container" ref={printRef}>
-                <h2 className="gallery__creature-title">{creature.name}</h2>
-                <svg width="133.33" height="233.33">
-                  <g transform="scale(.66)">
-                    {Legs[creature.legs]}
-                    {Bodies[creature.body]}
-                    {Heads[creature.head]}
-                  </g>
-                </svg>
-              </div>
-              <button
-                type="button"
-                onClick={handleDownloadImage}
-                className="gallery__download-button"
-              >
-                Download Creature
-              </button>
-              <div className="gallery__creature-likes-container">
-                <img
-                  src={upvote}
-                  alt="up arrow to increase likes"
-                  className="gallery__creature-upvote"
-                  onClick={handleUpvote}
-                  id={creature.id}
-                />
-                <p className="gallery__creature-likes">{creature.likes}</p>
-                <img
-                  src={downvote}
-                  alt="down arrow to decrease likes"
-                  className="gallery__creature-downvote"
-                  onClick={handleDownvote}
-                  id={creature.id}
-                />
-              </div>
-            </div>
+            <GalleryItem
+              creature={creature}
+              handleDownvote={handleDownvote}
+              handleUpvote={handleUpvote}
+            />
           );
         })}
       </section>
