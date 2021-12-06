6c03e2371033b5b79b3773c97d729dc27797da26
Hannah Park <hannahp056@gmail.com>
2021-12-05 20:53:52 -0500

    adjust home page create link and add pointer cursors

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <main className="home">
       <h2 className="home__title">Welcome to Creature Factory!</h2>
-      <section className="home__container">
-        <svg width="200" height="100" className="home__cat">
-          <CatHead />
-        </svg>
-        <Link to="/create">
+      <section>
+        <Link to="/create" className="home__container">
+          <svg width="200" height="100" className="home__cat">
+            <CatHead />
+          </svg>
           <button className="home__button">Get Started</button>
         </Link>
       </section>
