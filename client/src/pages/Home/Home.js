03320c434e45b758efe8541e3c4758c37a830c40
Hannah Park <hannahp056@gmail.com>
2021-12-05 19:39:26 -0500

    add cat head with animation to home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,19 @@
 import "./Home.scss";
 import { Link } from "react-router-dom";
+import CatHead from "../../components/Heads/CatHead";
 
 export default function Home() {
   return (
     <main className="home">
       <h2 className="home__title">Welcome to Creature Factory!</h2>
-      <Link to="/create">
-        <button className="home__button">Get Started</button>
-      </Link>
+      <section className="home__container">
+        <svg width="200" height="100" className="home__cat">
+          <CatHead />
+        </svg>
+        <Link to="/create">
+          <button className="home__button">Get Started</button>
+        </Link>
+      </section>
     </main>
   );
 }
